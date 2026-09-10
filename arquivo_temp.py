cb21543e31589c19df71536367d3f68ba7261fa5
André Santos <andresantos@MacBook-Pro-de-Andre.local>
2026-09-10 11:43:05 -0300

    alterado arquivo temp

--- arquivo_temp.py
+++ arquivo_temp.py
@@ -72,3 +72,68 @@ match varmatch:
         print("valor 3")
     case _:
         print("default!")
+
+lista = ["olá", 130]
+
+lista_exempo = ["oi", 3, True, True]
+lista_exempo.append(0.356)
+lista_exempo.append(lista)
+
+lista_exempo.extend(lista)
+
+lista_exempo.insert(1, False)
+
+lista_exempo = [1,2,3,4,5,6,7,8,9,10,1,2,3]
+
+slice1 = lista_exempo[2:]
+slice2 = lista_exempo[2:8]
+slice3 = lista_exempo[1::4]
+
+slice4 = lista_exempo[-1]
+slice5 = lista_exempo[-5:-2]
+
+slice6 = lista_exempo[::-1]
+slice7 = lista_exempo[-4:-10:-1]
+
+slice8 = lista_exempo[6:0:-1]
+
+print(len(slice8))
+
+lista_exempo = [1,2,3,4,5,6,7,8,9,10,1,2,3]
+
+lista_ex = lista_exempo.copy()
+
+lista_ex[0] = -1
+print(lista_exempo.count(4))
+print(lista_exempo)
+print(lista_ex)
+
+import copy
+
+l1 = [1,2,3]
+l2 = [4,5,6]
+l1.append(l2)
+l3 = copy.deepcopy(l1)
+l2[0] = 10
+l3[0] = -1
+print(l1)
+print(l3)
+
+i = 0
+while i < 3:
+    print(l1[i])
+    i += 1
+
+l4 = [6,7,8,9,10]
+
+for i in l4:
+    print(i)
+
+
+for i in l4:
+    print(i)
+    if i == 8:
+        break
+
+for idx in range(8,10):
+    print(idx)
