7ee200eed593d233daf79a7cdeb909213ded9c24
Niscoder <gianlucanisco@gmail.com>
2026-09-14 09:37:26 -0300

    commit

--- arquivo_temp.py
+++ arquivo_temp.py
@@ -1,74 +1,43 @@
-varb = 1
-varc = "abcd"
-vard = 0.233
-varf = True #False
+#lista = ["olá", 130]
 
-print(type(varb))
-print(type(varc))
-print(type(vard))
-print(type(varf))
+#lista_exemplo = ["oi", 3, True]
+#lista_exemplo.append(0.356)
+##lista_exemplo.append(lista)
+#lista_exemplo.extend(lista)
 
+#lista_exemplo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-## Operações numéricas
+#slice1 = lista_exemplo[2:]
+#slice2 = lista_exemplo[2:8]
+#slice3 = lista_exemplo[1::4]
 
-op1 = 1 + 3 #soma
-op2 = 1 - 3 #subtração
-op3 = 1 * 3 #multiplicação
-op4 = 1 / 3 #divisão
-op5 = 1 // 3 #divisão inteira
-op6 = 2 ** 2 #potência
-op7 = 3 % 5 #mod
+#slice4 = lista_exemplo[-1]
+#slice5 = lista_exemplo[-3:-1]
 
-calc = (3 + 5) / 7
+##slice6 = lista_exemplo[::-1]
+##slice7 = lista_exemplo[6:0:-1]
 
-## comparadores
+##print(slice1)
+##print(slice2)
+##print(slice3)
+##print(slice4)
+##print(slice5)   
+##print(slice6) 
+##print(slice7) 
 
-print(3 == 3) #igualdade
-print(3 != 2) #desigualdade
-print(5 > 3) #maior
-print(5 < 8) #menor
-print(5 >= 3) #maior igual
-print(5 <= 8) #menor igual
+##print(len(slice7))
 
-var1 = 3
-var2 = 3
+#i = 0
 
-print(var1 is var2) #verifica idêntico
-print(not var1 is var2) #verifica idêntico com negação
+#while i < 3:
+#    print(lista_exemplo[i])
+#    i +=1
 
-print(3 == 2 or 1 == 1) #operador ou
-print(3 == 2 and 1 == 1) #operador e
-print(2 == 2 or 3 == 2 and 1 == 1)
+for _ in range(int(input())):
+    name = input()
+    score = float(input())
+    
+array = [[name,score]]
+array.sort()
 
-print(not 2 == 2 or 3 == 2 and 1 == 1) #operador e executa primeiro
-
-# operações com strings
-
-print("abcd" == "abcd") #compara valor
-print("abcd" != "abcde") #compara valor
-print("abcda" > "abcd") #compara tamanho
-print("abc" < "abcd") #compara tamanho
-print("abcd" >= "abcd") #compara tamanho
-print("abcd" <= "abcd") #compara tamanho
-
-
-# condicional if
-if 3 < 2:
-    print("condição")
-elif 2 == 2:
-    print("elif")
-else:
-    print("condição falsa")
-
-varmatch = 6
-
-#condicional match (switch case)
-match varmatch:
-    case 1:
-        print("valor 1")
-    case 2:
-        print("valor 2")
-    case 3:
-        print("valor 3")
-    case _:
-        print("default!")
+print(array)
