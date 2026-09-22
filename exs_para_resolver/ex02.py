14ff030d0aeee6af49a72d128ac4c4369ae5d308
Niscoder <gianlucanisco@gmail.com>
2026-09-22 08:55:36 -0300

    commit10

--- exs_para_resolver/ex02.py
+++ exs_para_resolver/ex02.py
@@ -1,16 +1,7 @@
-# Crie um algoritmo em Python onde haverá uma função para realizar
-# operações matemáticas. Essa função deve implementar soma, subtração,
-# multiplicação e divisão. Como parâmetro, deve receber a informação de
-# qual operação deverá ser executado, e a operação deverá ocorrer em todos
-# os números recebidos por ela. Por exemplo, caso eu envie a informação de
-# soma e os números 2, 5, 8 e 10, o resultado deve ser 2+5+8+10. Não há limites
-# de números que devem ser passados como parâmetros, e se o tipo de operação não
-# ser informado, deve-se utilizar como padrão a soma. (Verificações: retornar
-# erro ao verificar que haverá divisão por 0)
-
-operation = input("Digite a operação desejada (soma, subtração, multiplicação, divisão): ")
-
 def calculate(operation="soma", *args):
+    
+    operation = operation.strip().lower()
+
     match operation:
         case "soma":
             return sum(args)
@@ -42,4 +33,12 @@ def calculate(operation="soma", *args):
             return result
 
         case _:
-            return sum(args)  # Padrão é soma
+            return "Erro: Operação inválida."
+
+op = input("Digite a operação desejada (soma, subtração, multiplicação, divisão): ")
+
+entrada_numeros = input("Digite os números separados por espaço: ")
+numeros = [float(x) for x in entrada_numeros.split()]
+
+resultado = calculate(op, *numeros)
+print(f"Resultado: {resultado}")  
